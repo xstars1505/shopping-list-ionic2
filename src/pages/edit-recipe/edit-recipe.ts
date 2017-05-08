1f23ff28c2a15188525797b095b3b0457bbe5896
minh le tran ba <mletranba@openwt.com>
2017-05-09 00:01:02 +0700

    Add Recipes page

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { NavParams, ActionSheetController, AlertController } from "ionic-angular/index";
+import { NavParams, ActionSheetController, AlertController, ToastController, NavController } from "ionic-angular/index";
 import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
+import { RecipesService } from "../../services/recipes";
 
 @Component({
   selector: 'page-edit-recipe',
@@ -12,9 +13,12 @@ export class EditRecipe implements OnInit{
   selectOptions = ['Easy', 'Medium', 'Hard'];
   recipeForm: FormGroup;
   constructor(
+      private navController: NavController,
       private navParams: NavParams,
       private actionSheetController: ActionSheetController,
-      private alertController: AlertController
+      private alertCtrl: AlertController,
+      private toastCtrl: ToastController,
+      private recipeService: RecipesService
   ) {  }
 
   ngOnInit() {
@@ -23,7 +27,16 @@ export class EditRecipe implements OnInit{
   }
 
   onSubmit() {
-
+    const value = this.recipeForm.value;
+    let ingredients = [];
+    if (value.ingredients.length > 0) {
+      ingredients = value.ingredients.map(name => {
+        return {name: name, amount: 1};
+      })
+    }
+    this.recipeService.addRecipe(value.title, value.description, value.difficulty, ingredients);
+    this.recipeForm.reset();
+    this.navController.popToRoot();
   }
 
   onManageIngredients() {
@@ -33,14 +46,26 @@ export class EditRecipe implements OnInit{
         {
           text: 'Add Ingredient',
           handler: () => {
-
+            this.createNewIngredientAlert().present();
           }
         },
         {
           text: 'Remove all Ingredients',
           role: 'destructive',
           handler: () => {
-
+            const fArray: FormArray = <FormArray>this.recipeForm.get('ingredients');
+            const len = fArray.length;
+            if (len > 0) {
+              for (let i=len-1; i >=0; i--) {
+                fArray.removeAt(i);
+              }
+              const toast = this.toastCtrl.create({
+                message: 'Items deleted!',
+                duration: 1000,
+                position: 'bottom' //default
+              });
+              toast.present();
+            }
           }
         },
         {
@@ -48,11 +73,12 @@ export class EditRecipe implements OnInit{
           role: 'cancel'
         }
       ]
-    })
+    });
+    actionSheet.present();
   }
 
   private createNewIngredientAlert() {
-    const newIngredientAlert = this.alertController.create({
+    return this.alertCtrl.create({
       title: 'Add Ingredient',
       inputs: [
         {
@@ -69,13 +95,26 @@ export class EditRecipe implements OnInit{
           text: 'Add',
           handler: data => {
             if (data.name.trim() == '' || data.name == null) {
-
+              const toast = this.toastCtrl.create({
+                message: 'Please enter a valid value!',
+                duration: 1000,
+                position: 'bottom' //default
+              });
+              toast.present();
+              return;
             }
-
+            (<FormArray>this.recipeForm.get('ingredients'))
+                .push(new FormControl(data.name, Validators.required));
+            const toast = this.toastCtrl.create({
+              message: 'Item added',
+              duration: 1000,
+              position: 'bottom' //default
+            });
+            toast.present();
           }
         }
       ]
-    })
+    });
   }
 
   private initializeForm() {
@@ -85,6 +124,6 @@ export class EditRecipe implements OnInit{
       'difficulty': new FormControl('Medium', Validators.required),
       'ingredients': new FormArray([])
     });
-
   }
+
 }
--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,19 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { EditRecipe } from "../edit-recipe/edit-recipe";
+import { RecipesService } from "../../services/recipes";
+import { Recipe } from "../../models/recipe";
 
 @Component({
   selector: 'page-recipes',
   templateUrl: 'recipes.html',
 })
 export class Recipes {
-
+  recipes: Recipe[];
   constructor(
-      private navCtrl: NavController
+      private navCtrl: NavController,
+      private recipeService: RecipesService
   ) {}
 
+  ionViewWillEnter() {
+    this.recipes = this.recipeService.getRecipe();
+  }
   onNewRecipe() {
     this.navCtrl.push(EditRecipe, {mode: 'New'});
+  }
+
+  onLoadRecipe() {
+
   }
 
 }
